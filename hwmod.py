d5c9c8c2e0f9bab8b4936657bec548430730b83a
DMITRY ULASAU <dmitrushok@gmail.com>
2022-07-08 10:07:34 -0400

    homework

---
+++ hwmod.py
@@ -0,0 +1,10 @@
+from math import pi
+
+def house_sqf (length, width):
+    """Calculates square footage"""
+    return length * width
+
+def circumference (radius):
+    """Calculates circle circumference"""
+    return round(2 * pi * radius, 2)
+
---
+++ mymod.py
@@ -0,0 +1,8 @@
+def say_hello():
+    print('hello')
+
+def say_goodbye():
+    print('goodbye')
+
+if __name__=='__main__':    
+    print("HeY YOU GuYSZ!")
